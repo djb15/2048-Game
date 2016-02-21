6078d45041aec6fc0b61f6ae527dfbeaab36b474
djb15 <dirk.judd@live.com>
2016-02-21 15:08:19 +0000

    random commit

---
+++ hello world/hello.cpp
@@ -0,0 +1,17 @@
+/*
+ * hello.cpp
+ *
+ *  Created on: 21 Feb 2016
+ *      Author: Dirk
+ */
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout << "Hey" << endl;
+	return 0;
+}
+
+
+
